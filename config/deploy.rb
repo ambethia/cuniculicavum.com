62f52eb4a98456a30935d3a5f3cb4d892d77602f
Jason L Perry <jasper@ambethia.com>
2008-11-08 00:36:28 -0500

    Production configuation

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+set :deploy_to, "/home/cuniculi/cuniculicavum.com"
+
+set :scm, :git
+set :repository,  "github.cuniculi_cavum:ambethia/cuniculicavum.com.git"
+set :git_enable_submodules, true
+set :deploy_via, :remote_cache
+
+role :app, "cuniculicavum.com"
+role :web, "cuniculicavum.com"
+role :db,  "cuniculicavum.com", :primary => true
+
+ssh_options[:username] = "cuniculi"
+set :run_method, :run
+
+namespace :deploy do
+  task :custom_symlinks do
+    run "ln -nfs #{shared_path}/system/database.yml #{release_path}/config/database.yml"
+  end
+  
+  desc "Restart Passenger"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with Passenger"
+    task t, :roles => :app do ; end
+  end
+end
+
+before "deploy:migrate", "deploy:custom_symlinks"
+after  "deploy:symlink", "deploy:custom_symlinks"
+after  "deploy",         "deploy:cleanup"
