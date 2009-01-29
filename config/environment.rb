2d430fa8a4a62cf2aeaefb38b97a82442c9eaf32
Jason L Perry <jasper@ambethia.com>
2009-01-29 14:48:20 -0500

    Argh

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "mislav-will_paginate", :lib => "will_paginate", :version => "~>2.1", :source => "http://gems.github.com/"
   config.gem "ruby-openid", :lib => "openid"
-  config.gem "RedCloth", :lib => "redcloth", :version => "3.0.3"
+  # config.gem "RedCloth", :lib => "redcloth", :version => "3.0.3"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
